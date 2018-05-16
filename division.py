053f72b4eb840da832c629c4f033f0056a3c9aa4
Васильев Артем Александрович <a.vasilev@ALMAZ.LOCAL>
2018-05-16 11:28:38 +0300

    Devision by zero fixed

--- division.py
+++ division.py
@@ -5,9 +5,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         'Divide one number by another')
     parser.add_argument('numerator', type=float, help='Numerator')
-    parser.add_argument('divisor', type=float, help='Divisor')
-    
+    parser.add_argument('divisor', type=float, help='Divisor') 
+
     args = parser.parse_args()
-    
-    print(args.numerator / args.divisor)
+    if args.divisor == 0:
+        print("Division by zero is prohibited!")
+    else: 
+        print(args.numerator / args.divisor)
 
